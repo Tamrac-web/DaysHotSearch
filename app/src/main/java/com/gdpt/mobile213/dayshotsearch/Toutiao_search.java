bbdf62db05f37912ff4b113be676f62b17df8dae
Carl <cbzone7@outlook.com>
2022-06-27 11:54:59 +0800

    添加知乎热搜类 头条热搜类

---
+++ app/src/main/java/com/gdpt/mobile213/dayshotsearch/Toutiao_search.java
@@ -0,0 +1,24 @@
+package com.gdpt.mobile213.dayshotsearch;
+
+public class Toutiao_search {
+
+
+    private String url;
+    private String word;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
