cc2b2b00d7a25866b318186c39f3d414ddf8ca78
Carl <cbzone7@outlook.com>
2022-06-28 08:53:37 +0800

    0608Test

---
+++ app/src/main/java/com/gdpt/mobile213/dayshotsearch/DataManagers.java
@@ -0,0 +1,11 @@
+package com.gdpt.mobile213.dayshotsearch;
+
+public class DataManagers {
+
+    static final String ZHIHU_SEARCH ="https://cdn.jsdelivr.net/gh/huqi-pr/trending-in-one/raw/zhihu-search/";
+    static final String TOUTIAO_SEARCH = "https://cdn.jsdelivr.net/gh/huqi-pr/trending-in-one/raw/toutiao-search/";
+    static final String WEIBO_SEARCH = "https://cdn.jsdelivr.net/gh/huqi-pr/trending-in-one/raw/weibo-search/";
+    static final String ZHIHU_QUESTIONS = "https://cdn.jsdelivr.net/gh/huqi-pr/trending-in-one/raw/zhihu-questions/";
+    static final String ZHIHU_VIDEO= "https://cdn.jsdelivr.net/gh/huqi-pr/trending-in-one/raw/zhihu-video/";
+
+}
--- app/src/main/java/com/gdpt/mobile213/dayshotsearch/MainActivity.java
+++ app/src/main/java/com/gdpt/mobile213/dayshotsearch/MainActivity.java
@@ -1,14 +1,63 @@
 package com.gdpt.mobile213.dayshotsearch;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    OkHttpClient okHttpClient;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        initView();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Date date = new Date();
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+                //获取当前时间
+                String today = formatter.format(date) + ".json";
+
+
+                String url = DataManagers.TOUTIAO_SEARCH + today;
+                Log.i("TAG",url);
+
+
+
+            }
+        });
+    }
+
+    private void initView() {
+        textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
     }
 }
